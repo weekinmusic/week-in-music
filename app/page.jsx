4378fb93c5c669dad2ea36d17e47d4ade78cf339
weekinmusic <info@weekinmusic.com>
2025-10-23 17:41:23 -0500

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+```jsx
+import WeekInMusicApp from "@/components/WeekInMusicApp";
+
+export default function HomePage() {
+  return (
+    <main className="p-6 max-w-7xl mx-auto">
+      <WeekInMusicApp />
+    </main>
+  );
+}
+```
