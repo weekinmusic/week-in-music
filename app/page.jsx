dff99956d2c5e8abba37def3a8897a6825b2ad85
weekinmusic <info@weekinmusic.com>
2025-10-23 18:20:10 -0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import WeekInMusicApp from "../components/WeekInMusicApp";
 
 export default function HomePage() {
   return (
-    <main className="p-6 max-w-7xl mx-auto">
+    <main style={{ padding: 24, maxWidth: 1000, margin: "0 auto" }}>
       <WeekInMusicApp />
     </main>
   );
