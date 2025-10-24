9496c5efc2bf7645f6efaecfe7c760238214fe92
weekinmusic <info@weekinmusic.com>
2025-10-23 19:13:29 -0500

    Update WeekInMusicApp.jsx

--- components/WeekInMusicApp.jsx
+++ components/WeekInMusicApp.jsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 
 export default function WeekInMusicApp() {
-  const [events, setEvents] = useState([
+  const [events] = useState([
     { id: 1, title: "Acoustic Night: The Rivets", venue: "Bluebird Bar", date: "2025-10-26", time: "19:00", artist: "The Rivets", paid: false },
     { id: 2, title: "Open Mic", venue: "The Garage", date: "2025-10-28", time: "20:00", artist: "Open Stage", paid: true }
   ]);
