423a52870a802d43a5e91d23966928809e6fcee8
weekinmusic <info@weekinmusic.com>
2025-10-23 20:55:34 -0500

    Update WeekInMusicApp.jsx

--- components/WeekInMusicApp.jsx
+++ components/WeekInMusicApp.jsx
@@ -87,7 +87,7 @@ function Logo() {
           alt="Week in Music"
           width={64}
           height={64}
-          className="w-16 h-16 object-cover contain rounded-xl"
+          className="w-14 h-14 object-cover contain rounded-xl"
           priority
           onError={() => setErr(true)}
         />
